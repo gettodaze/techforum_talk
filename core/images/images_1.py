2f4b87863ccc504f8daca432ff9adb53af309521
John McCloskey <jmccloskey2@wisc.edu>
2022-04-19 14:01:57 -0700

    add images part of the presentation

--- core/images/images_1.py
+++ core/images/images_1.py
@@ -1,34 +1,33 @@
-import os
 from pathlib import Path
 import shutil
-import subprocess
 from PIL import Image
-from datetime import datetime
 
-input_path = Path("/home/mccloskey/src/techforum_talk/input")
-output_path = Path("/home/mccloskey/src/techforum_talk/output")
+input_path = Path("input")
+output_path = Path("output")
 
 for path in input_path.iterdir():
+    if path.suffix != ".jpg":
+        continue
     image = Image.open(path)
     # subprocess.run(["code", path]) # for WSL, open in vscode server
-    image.show() # for Windows/Linux/Mac
+    image.show()  # for Windows/Linux/Mac
     while True:
         output_path.mkdir(parents=True, exist_ok=True)
-        new_name = input(f'Current name: {path.stem}.\nNew name? ')
-        new_path = output_path / f'{new_name}{path.suffix}'
+        new_name = input(f"Current name: {path.stem}.\nNew name? ")
+        new_path = output_path / f"{new_name}{path.suffix}"
 
         msg = f"Copying {path} to {new_path}. "
         if new_path.exists():
-            msg += f'WARNING: {new_path} already exists. '
-        answer = input(msg+'Okay? [y/n]').lower()
-        if answer in {'y', 'yes'}:
+            msg += f"WARNING: {new_path} already exists. "
+        answer = input(msg + "Okay? [y/n]").lower()
+        if answer in {"y", "yes"}:
             shutil.copyfile(path, new_path)
             print(f"Copied {path} to {new_path}. ")
             break
-        elif answer in {'n', 'no'}:
+        elif answer in {"n", "no"}:
             pass
-        elif answer in {'s', 'skip'}:
-            print('Not copying.')
+        elif answer in {"s", "skip"}:
+            print("Not copying.")
             break
         else:
-            print(f'Invalid answer: {answer}. Must be [y]es, [n]o or [s]kip.')
+            print(f"Invalid answer: {answer}. Must be [y]es, [n]o or [s]kip.")
--- input/presentation.md
+++ input/presentation.md
@@ -33,3 +33,45 @@ END
 ![bad_images_nautilus](images/bad_images_nautilus.png)
 - You could do this by hand, but if you have hundreds of images, that could take a long time.
 ## You can write a simple program to do make this a lot easier
+- Setup:
+CODE
+from pathlib import Path
+import shutil
+from PIL import Image
+
+input_path = Path("input")
+output_path = Path("output")
+END
+
+## Main Loop:
+CODE
+for path in input_path.iterdir():
+    if path.suffix != ".jpg":
+        continue
+    image = Image.open(path)
+    # subprocess.run(["code", path]) # for WSL, open in vscode server
+    image.show()  # for Windows/Linux/Mac
+    while True:
+        output_path.mkdir(parents=True, exist_ok=True)
+        new_name = input(f"Current name: {path.stem}.\nNew name? ")
+        new_path = output_path / f"{new_name}{path.suffix}"
+
+        msg = f"Copying {path} to {new_path}. "
+        if new_path.exists():
+            msg += f"WARNING: {new_path} already exists. "
+        answer = input(msg + "Okay? [y/n]").lower()
+        if answer in {"y", "yes"}:
+            shutil.copyfile(path, new_path)
+            print(f"Copied {path} to {new_path}. ")
+            break
+        elif answer in {"n", "no"}:
+            pass
+        elif answer in {"s", "skip"}:
+            print("Not copying.")
+            break
+        else:
+            print(f"Invalid answer: {answer}. Must be [y]es, [n]o or [s]kip.")
+END
+## Example:
+![renaming_springrolls](images/renaming_springrolls.png)
+
