2f6c83838a66081c17af9c0c6a2b17bcbfa80cae
John McCloskey <jmccloskey2@wisc.edu>
2022-04-19 12:51:42 -0700

    add jlog 1

---
+++ core/jlog/jlog_1.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+OUTPATH = "/home/mccloskey/src/john/techforum_webscraping/output/my_logs.txt"
+
+
+def main():
+    while True:
+        user_input = input("log: ")
+        log = f"{datetime.now()} - {user_input}"
+        with open(OUTPATH, mode="a+") as f:
+            f.write(f"{log}\n")
+        print(log)
+
+
+if __name__ == "__main__":
+    main()
