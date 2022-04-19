1cc9ea5f50bab1897a9db0236b9cb2a382e0888d
John McCloskey <jmccloskey2@wisc.edu>
2022-04-19 10:12:26 -0700

    clean ebook 1

--- core/ebook/ebook_2.py
+++ core/ebook/ebook_2.py
@@ -12,19 +12,35 @@ INPUT_DIR = Path("/home/mccloskey/src/john/techforum_webscraping/input")
 
 
 def main(title: str, input_path: Path) -> Path:
-    # make chapter
+    OUTPUT_DIR.mkdir(exist_ok=True)
     chapter = epub.EpubHtml(
         title=title, content=input_path.read_text(), file_name="file1.xhtml"
     )
 
-    # make book
     book = epub.EpubBook()
     book.add_item(chapter)
+
+    # # define Table Of Contents
+    # book.toc = ((epub.Section("Simple book"), (chapter,)),)
+
+    # # add default NCX and Nav file
+    # book.add_item(epub.EpubNcx())
+    # book.add_item(epub.EpubNav())
+
+    # # define CSS style
+    # style = "BODY {color: white;}"
+    # nav_css = epub.EpubItem(
+    #     uid="style_nav", file_name="style/nav.css", media_type="text/css", content=style
+    # )
+
+    # # add CSS file
+    # book.add_item(nav_css)
+
+    # basic spine
     book.spine = [chapter]
 
-    # write book
-    OUTPUT_DIR.mkdir(exist_ok=True)
     output_path = OUTPUT_DIR / f"{title}.epub"
+
     epub.write_epub(output_path, book)
 
     return output_path
