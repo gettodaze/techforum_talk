749174ccb5f553998d0388462b7799d0d836e954
John McCloskey <jmccloskey2@wisc.edu>
2022-04-19 12:14:18 -0700

    add jisho 1

---
+++ core/jisho/jisho_1.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+import json
+
+import requests
+import typing as tp
+
+BASE_URL = "https://jisho.org/api/v1/search"
+
+
+def call_api(keyword: str) -> dict[str, tp.Any]:
+    response = requests.get(f"{BASE_URL}/words?keyword={keyword}")
+    response.raise_for_status()
+    return response.json()
+
+
+def main() -> None:
+    keyword = input("Keyword? ")
+    print(json.dumps(call_api(keyword), indent=2))
+
+
+if __name__ == "__main__":
+    main()
