314fd04b0d8f697207bcfe214ca216d2fe9a4013
John McCloskey <jmccloskey2@wisc.edu>
2022-04-20 19:13:13 -0700

    add images 0

---
+++ apps/images/images_0.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import shutil
+from PIL import Image
+
+input_path = Path("input")
+output_path = Path("output")
+output_path.mkdir(exist_ok=True)
+
+for path in input_path.iterdir():
+    if path.suffix != ".jpg":
+        continue
+
+    Image.open(path).show()
+
+    new_name = input(f"Current name: {path.stem}.\nNew name? ")
+    new_path = output_path / f"{new_name}{path.suffix}"
+
+    shutil.copyfile(path, new_path)
+    print(f"Copied {path} to {new_path}. ")
